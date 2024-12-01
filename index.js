9d092aaaa8e6b7a54b34e3258c99fd4effa161d4
AndreyKarm <karmandreii@gmail.com>
2024-12-01 00:41:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ bot.on('message', async (ctx) => {
     const url = ctx.message.text;
 
     if (regex.test(url)) {
+        const message = await bot.telegram.sendMessage(chatId, "Downloading video...");
+        const messageId = message.message_id;
         const data = await download(url);
-        if (url.includes('vm.tiktok') || url.includes('instagram.com/reel')) {      // for vidoes
+        if (!data) {
+            await bot.telegram.editMessageText(chatId, messageId, undefined, "Error downloading video.");
+            return;
+        }
+        if (url.includes('vm.tiktok') || url.includes('instagram.com/reel')) {      // for videos
             try {
+                await bot.telegram.editMessageText(chatId, messageId, undefined, "Sending video...");
                 await bot.telegram.sendVideo(chatId, data.url);
             }
             catch (error) {
-                await bot.telegram.sendMessage(chatId, "Error sending video.");
+                await bot.telegram.editMessageText(chatId, messageId, undefined, "Error sending video.");
             }
         }
         else if (url.includes('vt.tiktok') || url.includes('instagram.com/p')) {     // for images
-            try{
+            try {
+                await bot.telegram.editMessageText(chatId, messageId, undefined, "Sending image...");
                 const mediaGroup = data.picker.map(item => ({
                     type: item.type,
                     media: item.url
@@ -29,11 +37,11 @@ bot.on('message', async (ctx) => {
                 await bot.telegram.sendMediaGroup(chatId, mediaGroup);
             }
             catch (error) {
-                await bot.telegram.sendMessage(chatId, "Error sending image.");
+                await bot.telegram.editMessageText(chatId, messageId, undefined, "Error sending image.");
             }
         }
         else {
-            await bot.telegram.sendMessage(chatId, "Unsupported link type.");
+            await bot.telegram.editMessageText(chatId, messageId, undefined, "Unsupported link type.");
         }
     }
 });
