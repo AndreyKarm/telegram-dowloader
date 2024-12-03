889714f5e9b3dcd27c5a1dc2520a98071353c86f
AndreyKarm <karmandreii@gmail.com>
2024-12-03 13:55:58 +0000

    Added error messages support
    Yippie

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const regex = /https?:\/\/(www\.)?(vm\.)?(vt\.)?(tiktok\.com|instagram\.com\/(reel|p))/;
 
+const errorMessages = JSON.parse(fs.readFileSync(path.join(__dirname, 'error.json'), 'utf8'));
+
 bot.on('message', async (ctx) => {
     const chatId = ctx.chat.id;
     const url = ctx.message.text;
 
     if (regex.test(url)) {
-        try {
-            const data = await download(url);
-            if (!data) {
-                await bot.telegram.sendMessage(chatId, "Error downloading video.");
-                return;
+        const data = await download(url, chatId);
+        if (!data) return;
+        if (url.includes('vm.tiktok') || url.includes('instagram.com/reel')) {      // for videos
+            try {
+                await bot.telegram.sendVideo(chatId, data.url);
             }
-            if (url.includes('vm.tiktok') || url.includes('instagram.com/reel')) {      // for videos
-                try {
-                    await bot.telegram.sendVideo(chatId, data.url);
-                }
-                catch (error) {
-                    await bot.telegram.sendMessage(chatId, "Error sending video.");
-                }
+            catch (error) {
+                await bot.telegram.sendMessage(chatId, "Error sending video.");
             }
-            else if (url.includes('vt.tiktok') || url.includes('instagram.com/p')) {     // for images
-                try {
-                    const mediaGroup = data.picker.map(item => ({
-                        type: item.type,
-                        media: item.url
-                    }));
-                    await bot.telegram.sendMediaGroup(chatId, mediaGroup);
-                }
-                catch (error) {
-                    await bot.telegram.sendMessage(chatId, "Error sending image.");
-                }
+        }
+        if (url.includes('vt.tiktok') || url.includes('instagram.com/p')) {     // for images
+            try {
+                const mediaGroup = data.picker.map(item => ({
+                    type: item.type,
+                    media: item.url
+                }));
+                await bot.telegram.sendMediaGroup(chatId, mediaGroup);
             }
-            else {
-                await bot.telegram.sendMessage(chatId, "Unsupported link type.");
+            catch (error) {
+                await bot.telegram.sendMessage(chatId, "Error sending image.");
             }
-        } catch (error) {
-            await bot.telegram.sendMessage(chatId, "Error downloading video.");
+        }
+        else {
+            await bot.telegram.sendMessage(chatId, "Unsupported link type.");
         }
     }
 });
 
-async function download(url) {
+function getErrorMessage(code) {
+    const formattedCode = code.startsWith('error.') ? code.slice(6) : code;
+    return errorMessages[formattedCode] || 'Unknown error';
+}
+
+async function download(url, chatId) {
     try {
         const response = await axios.post(process.env.API_URL, {
             url: url,
@@ -72,8 +74,7 @@ async function download(url) {
         });
         return response.data;
     } catch (error) {
-        console.error('Error downloading:', error.response ? error.response.data : error.message);
-        throw error;
+        await bot.telegram.sendMessage(chatId, `Error downloading: ${getErrorMessage(error.response.data.error.code)}`);
     }
 }
 
